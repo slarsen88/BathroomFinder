e05a61d20dbb747391e4665d7b1f0cba742f8f2e
Alex <alexkourks@gmail.com>
2020-04-17 17:43:06 -0700

    adding more updates to poopynavbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,35 @@
-import React from 'react'
-import './App.css'
+import React, {Component } from 'react';
+import Navbar from './components/Navbar';
+import GlobalStyle from './styles/Global';
+import './App.css';
 
-function App() {
-  return (
-    <div className='App'>
-      <h1>hello world</h1>
-    </div>
-  )
+// function App() {
+//   return <>
+//   <NavBar/>
+//   <GlobalStyle />
+//   </>
+// }
+
+class App extends Component {
+  state = {
+    navbarOpen: false
+  }
+
+  handleNavbar = () => {
+    this.setState({ navbarOpen: !this.state.navbarOpen });
+  }
+
+  render() {
+    return (
+      <>
+      <Navbar
+        navbarState={this.state.navbarOpen}
+        handleNavbar={this.handleNavbar}
+        />
+        <GlobalStyle />
+      </>
+    )
+  }
 }
+
 export default App
