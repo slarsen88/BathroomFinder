26b1a322c35bbefeeed95a8586aa68dbecbdcbf2
Stuart Larsen <stuart.larsen@outlook.com>
2020-04-18 08:23:27 -0700

    adding packages to server and setup basic route to test if it was working

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const dotenv = require('dotenv')
+dotenv.config({ path: './config/config.env' })
+
+const app = express()
+
+const PORT = process.env.PORT || 3001
+
+app.get('/', (req, res) => {
+  res.send('API running!')
+})
+
+app.listen(PORT, () => {
+  console.log(`App is listening on port ${PORT}`)
+})
